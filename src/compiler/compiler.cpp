71592b58dc55b6983c812035ee31450264397588
Radoslav Petkov <radipirate@gmail.com>
2017-05-18 09:00:18 +0300

    Implement () eval Improve unit tests

--- src/compiler/compiler.cpp
+++ src/compiler/compiler.cpp
@@ -24,118 +24,61 @@ NFA RegularExpressionCompiler::compile(RegularExpression expression) {
             NFA nfa = NFA(start, end, nfaCounter);
             nfaCounter++;
             constructedNFA.push(nfa);
-        } else if (parsedToken->isOperator()) {
+        }
+
+        if (parsedToken->isOperator()) {
             if (operators.empty() || parsedToken->get() == Operator::LEFT_P) {
+                Operator op = Operator(parsedToken->get());
+                operators.push(op);
+            } else if (parsedToken->get() == Operator::RIGHT_P) {
+                // we matched end of (), we should eval all operators between ( and )
+
+                while (!operators.empty() && operators.top().getValue() != Operator::LEFT_P) {
+                    Operator op = operators.top();
+                    NFA nfa = constructedNFA.top();
+
+                    constructedNFA.pop();
+                    operators.pop();
+
+                    switch (op.getValue()) {
+                        case Operator::STAR: {
+                            nfa.kleene();
+                            constructedNFA.push(nfa);
+                            break;
+                        }
+                        case Operator::UNION: {
+                            NFA other = constructedNFA.top();
+                            constructedNFA.pop();
+                            nfa._union(other);
+                            constructedNFA.push(nfa);
+                            break;
+                        }
+                        case Operator::CONCAT: {
+                            NFA previous = constructedNFA.top();
+                            constructedNFA.pop();
+                            previous.concat(nfa);
+                            constructedNFA.push(previous);
+                            break;
+                        }
+                    }
+                }
+
+                operators.pop();
+
+            } else {
+                // eval if we can and push new op
+
+                //missing eval code here
+
                 Operator op = Operator(parsedToken->get());
                 operators.push(op);
             }
         }
 
-        //else if (parsedToken->get() == Operator::RIGHT_P) {
-//                // we matched () we should eval all operators between ( and )
-//
-//                while (!operators.empty() && operators.top().getValue() != Operator::LEFT_P) {
-//                    Operator op = operators.top();
-//                    NFA nfa = constructedNFA.top();
-//
-//                    constructedNFA.pop();
-//                    operators.pop();
-//
-//                    switch (op.getValue()) {
-//                        case Operator::STAR: {
-//                            nfa.kleene();
-//                            constructedNFA.push(nfa);
-//                            break;
-//                        }
-//                        case Operator::UNION: {
-//                            NFA other = constructedNFA.top();
-//                            constructedNFA.pop();
-//                            nfa._union(other);
-//                            constructedNFA.push(nfa);
-//                            break;
-//                        }
-//                        case Operator::CONCAT: {
-//                            NFA previous = constructedNFA.top();
-//                            constructedNFA.pop();
-//                            previous.concat(nfa);
-//                            constructedNFA.push(previous);
-//                            break;
-//                        }
-//                    }
-//                }
-//
-//                operators.pop();
-//            } else {
-//                // eval everything
-//                // and put it in the stack
-//
-//                while (!operators.empty()) {
-//                    Operator op = operators.top();
-//                    NFA nfa = constructedNFA.top();
-//
-//                    constructedNFA.pop();
-//                    operators.pop();
-//
-//                    switch (op.getValue()) {
-//                        case Operator::STAR: {
-//                            nfa.kleene();
-//                            constructedNFA.push(nfa);
-//                            break;
-//                        }
-//                        case Operator::UNION: {
-//                            NFA other = constructedNFA.top();
-//                            constructedNFA.pop();
-//                            nfa._union(other);
-//                            constructedNFA.push(nfa);
-//                            break;
-//                        }
-//                        case Operator::CONCAT: {
-//                            NFA previous = constructedNFA.top();
-//                            constructedNFA.pop();
-//                            previous.concat(nfa);
-//                            constructedNFA.push(previous);
-//                            break;
-//                        }
-//                    }
-//                }
-//
-//                Operator op = Operator(parsedToken->get());
-//                operators.push(op);
-//            }
-//        } else {
-//            // if it is not operator or input letter what is it?
-//        }
         delete parsedToken;
     }
-    while (!operators.empty()) {
-        Operator op = operators.top();
-        NFA nfa = constructedNFA.top();
 
-        constructedNFA.pop();
-        operators.pop();
-
-        switch (op.getValue()) {
-            case Operator::STAR: {
-                nfa.kleene();
-                constructedNFA.push(nfa);
-                break;
-            }
-            case Operator::UNION: {
-                NFA other = constructedNFA.top();
-                constructedNFA.pop();
-                nfa._union(other);
-                constructedNFA.push(nfa);
-                break;
-            }
-            case Operator::CONCAT: {
-                NFA previous = constructedNFA.top();
-                constructedNFA.pop();
-                previous.concat(nfa);
-                constructedNFA.push(previous);
-                break;
-            }
-        }
-    }
+    processOperator(operators, constructedNFA);
 
     delete expr;
     return constructedNFA.top();
@@ -191,5 +134,33 @@ RegularExpression *RegularExpressionCompiler::preprocessForConcat(RegularExpress
 }
 
 void RegularExpressionCompiler::processOperator(std::stack<Operator> &operators, std::stack<NFA> &constructedNFA) {
+    while (!operators.empty()) {
+        Operator op = operators.top();
+        NFA nfa = constructedNFA.top();
 
+        constructedNFA.pop();
+        operators.pop();
+
+        switch (op.getValue()) {
+            case Operator::STAR: {
+                nfa.kleene();
+                constructedNFA.push(nfa);
+                break;
+            }
+            case Operator::UNION: {
+                NFA other = constructedNFA.top();
+                constructedNFA.pop();
+                nfa._union(other);
+                constructedNFA.push(nfa);
+                break;
+            }
+            case Operator::CONCAT: {
+                NFA previous = constructedNFA.top();
+                constructedNFA.pop();
+                previous.concat(nfa);
+                constructedNFA.push(previous);
+                break;
+            }
+        }
+    }
 }
--- test/compiler/compiler-test.cpp
+++ test/compiler/compiler-test.cpp
@@ -1,16 +1,12 @@
 #include "compiler/compiler.h"
 #include "gtest/gtest.h"
 
-RegularExpressionCompiler createCompiler(char regEx[]) {
-
-}
-
 TEST(RegularExpressionCompiler, ItShouldCompileAToNFA) {
     char regEx[] = "a";
     RegularExpressionCompiler regularExpressionCompiler = RegularExpressionCompiler();
     NFA result = regularExpressionCompiler.compile(regEx);
     ASSERT_EQ(result.getStates().size(), 2);
-    ASSERT_EQ(result.getStartState()->getTransitions()[0]->getLabel(),'a');
+    ASSERT_EQ(result.getStartState()->getTransitions()[0]->getLabel(), 'a');
 }
 
 TEST(RegularExpressionCompiler, ItShouldEvalConcat) {
@@ -36,19 +32,35 @@ TEST(RegularExpressionCompiler, ItShouldEvalKleeneStar) {
 
     ASSERT_EQ(result.getStates().size(), 4);
 }
-//
-//TEST(RegularExpressionCompiler, ItShouldEvalEverythingBetweenLeftPRightP1) {
-//    char regEx[] = "(ab)";
-//    RegularExpressionCompiler regularExpressionCompiler = RegularExpressionCompiler();
-//    NFA result = regularExpressionCompiler.compile(regEx);
-//
-//    ASSERT_EQ(result.getStates().size(), 4);
-//}
-//
-//TEST(RegularExpressionCompiler, ItShouldEvalEverythingBetweenLeftPRightP2) {
-//    char regEx[] = "(a*b)";
-//    RegularExpressionCompiler regularExpressionCompiler = RegularExpressionCompiler();
-//    NFA result = regularExpressionCompiler.compile(regEx);
-//
-//    ASSERT_EQ(result.getStates().size(), 4);
-//}
+
+TEST(RegularExpressionCompiler, ItShouldEvalComplexUnion) {
+    char regEx[] = "a|b*";
+    RegularExpressionCompiler regularExpressionCompiler = RegularExpressionCompiler();
+    NFA result = regularExpressionCompiler.compile(regEx);
+
+    ASSERT_EQ(result.getStates().size(), 8);
+}
+
+TEST(RegularExpressionCompiler, ItShouldEvalEverythingBetweenLeftPRightP1) {
+    char regEx[] = "(ab)";
+    RegularExpressionCompiler regularExpressionCompiler = RegularExpressionCompiler();
+    NFA result = regularExpressionCompiler.compile(regEx);
+
+    ASSERT_EQ(result.getStates().size(), 4);
+}
+
+TEST(RegularExpressionCompiler, ItShouldEvalEverythingBetweenLeftPRightP2) {
+    char regEx[] = "(a*)";
+    RegularExpressionCompiler regularExpressionCompiler = RegularExpressionCompiler();
+    NFA result = regularExpressionCompiler.compile(regEx);
+
+    ASSERT_EQ(result.getStates().size(), 4);
+}
+
+TEST(RegularExpressionCompiler, ItShouldEvalEverythingBetweenLeftPRightP3) {
+    char regEx[] = "(a*b)";
+    RegularExpressionCompiler regularExpressionCompiler = RegularExpressionCompiler();
+    NFA result = regularExpressionCompiler.compile(regEx);
+
+    ASSERT_EQ(result.getStates().size(), 6);
+}
